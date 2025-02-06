401fd3f90cc3273665f55bf42f0832ac5a11e409
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2025-02-06 17:33:12 +0530

    if-elsif-else statements in PL/SQL

---
+++ 03_control_structures/if_ctrl_structure.sql
@@ -0,0 +1,14 @@
+-- run only once
+SET serveroutput ON;
+
+DECLARE
+	v_num NUMBER := 20;
+BEGIN
+	IF (MOD(v_num, 2) = 0) AND (v_num IS NOT NULL) THEN
+		dbms_output.put_line(v_num || ' is even.');
+	ELSIF (MOD(v_num, 2) <> 0) AND (v_num IS NOT NULL) THEN
+		dbms_output.put_line(v_num || ' is odd.');
+	ELSE
+		dbms_output.put_line('Please enter a valid number.');
+	END IF;
+END;
