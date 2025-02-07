85f864699bca3c6a50152480a0be0c3b69573a6f
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2025-02-07 15:13:54 +0530

    using the sql data in pl/sql programs
    that's the reason of this procedural language!

---
+++ 04_sql_in_plsql/demo.sql
@@ -0,0 +1,17 @@
+-- run the below comand only once
+SET serveroutput ON;
+
+DECLARE
+	v_name VARCHAR2(20);
+	v_salary hr.employees.salary%TYPE;
+	v_employee_id hr.employees.employee_id%TYPE := 100;
+BEGIN
+    -- only one record returns
+	SELECT (first_name || ' ' || last_name), salary
+	INTO v_name, v_salary
+	FROM hr.employees
+	WHERE employee_id = v_employee_id;
+
+	dbms_output.put_line('The salary of ' || v_name || ' is: ' || v_salary);
+END;
+/
