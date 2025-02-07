91073f431c2fc5c9de05a794e81f2b99bc086e93
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2025-02-07 15:55:11 +0530

    Update delete_demo.sql

--- 04_sql_in_plsql/delete_demo.sql
+++ 04_sql_in_plsql/delete_demo.sql
@@ -2,8 +2,6 @@
 SET serveroutput ON;
 
 -- after the INSERT [UPDATE optional] demo run the below program
-DECLARE
-	v_salary_inc hr.employees.salary%TYPE := 100;
 BEGIN
 	FOR i IN 206..216 LOOP
 		DELETE FROM emp
