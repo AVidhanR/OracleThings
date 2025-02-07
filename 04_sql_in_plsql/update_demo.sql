dba852b038814c56f53b534b4b83e9f2cb2eafa1
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2025-02-07 15:47:58 +0530

    update demo in plsql
    only perform these operations after the insert demo operations

---
+++ 04_sql_in_plsql/update_demo.sql
@@ -0,0 +1,14 @@
+-- run the below SET comand only once
+SET serveroutput ON;
+
+-- after the INSERT demo run the below program
+DECLARE
+	v_salary_inc hr.employees.salary%TYPE := 100;
+BEGIN
+	FOR i IN 206..216 LOOP
+		UPDATE emp
+    	SET salary = salary + v_salary_inc
+		WHERE employee_id = i;
+	END LOOP;
+END;
+/
