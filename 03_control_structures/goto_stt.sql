a171745d937b8f2b3ad354ce7956c7e910bb2575
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2025-02-07 14:38:53 +0530

    goto statement use-case with an example of is-prime program

---
+++ 03_control_structures/goto_stt.sql
@@ -0,0 +1,27 @@
+-- run the below command once
+SET serveroutput ON;
+
+DECLARE
+	v_num NUMBER NOT NULL := 17;
+	v_is_prime BOOLEAN := true;
+BEGIN
+	IF (v_num <= 1) THEN
+    	dbms_output.put_line('Enter a valid number');
+	END IF;
+
+	FOR i IN 2..(v_num-1) LOOP
+		IF (v_num MOD i = 0) THEN
+			v_is_prime := false;
+			dbms_output.put_line(v_num || ' is not a prime number.');
+			GOTO end_point;
+		END IF;
+	END LOOP;
+
+	IF (v_is_prime) THEN
+        dbms_output.put_line(v_num || ' is a prime number.');
+	END IF;
+
+	<<end_point>>
+	dbms_output.put_line('Check complete.');
+END;
+/
