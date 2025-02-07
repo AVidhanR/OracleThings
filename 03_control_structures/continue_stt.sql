ce24092b122621aead73e21b755ab02c77c1e3fc
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2025-02-07 12:38:18 +0530

    continue statement (stt) is very powerful in nested loops

---
+++ 03_control_structures/continue_stt.sql
@@ -0,0 +1,41 @@
+-- run the below command once
+SET serveroutput ON;
+
+DECLARE
+	v_inner NUMBER NOT NULL := 1;
+BEGIN
+    <<outer_loop>>
+	FOR v_outer IN 1..10 LOOP
+		dbms_output.put_line('The outer loop var: ' || v_outer);
+		v_inner := 1;
+
+		<<inner_loop>>
+		WHILE (v_inner * v_outer < 15) LOOP
+			v_inner := v_inner + 1;
+			CONTINUE WHEN (MOD(v_inner * v_outer, 3) = 0);
+			dbms_output.put_line(' The inner loop var: ' || v_inner);
+		END LOOP inner_loop;
+
+	END LOOP outer_loop;
+END;
+/
+
+-- using label in CONTINUE statement
+DECLARE
+	v_inner NUMBER NOT NULL := 1;
+BEGIN
+    <<outer_loop>>
+	FOR v_outer IN 1..10 LOOP
+		dbms_output.put_line('The outer loop var: ' || v_outer);
+		v_inner := 1;
+
+		<<inner_loop>>
+		WHILE (v_inner * v_outer < 15) LOOP
+			v_inner := v_inner + 1;
+			CONTINUE outer_loop WHEN v_inner = 10;
+			dbms_output.put_line(' The inner loop var: ' || v_inner);
+		END LOOP inner_loop;
+
+	END LOOP outer_loop;
+END;
+/
