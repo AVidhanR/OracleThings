e23671867e7f9b3b2e462be756c412bcec0aab48
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2025-02-07 11:55:25 +0530

    Create for_loop.sql

---
+++ 03_control_structures/for_loop.sql
@@ -0,0 +1,14 @@
+-- below command, run only once
+SET serveroutput ON;
+
+BEGIN
+	FOR i IN 1..3 LOOP
+		dbms_output.put_line(i);
+	END LOOP;
+END;
+/
+BEGIN
+	FOR i IN REVERSE 1..3 LOOP
+		dbms_output.put_line(i);
+	END LOOP;
+END;
