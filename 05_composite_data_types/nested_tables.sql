06728cc422ae8b1e60fc196371c033a253634996
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2025-02-10 18:31:04 +0530

    Create nested_tables.sql

---
+++ 05_composite_data_types/nested_tables.sql
@@ -0,0 +1,35 @@
+-- run the below SET comand once
+SET serveroutput ON;
+
+DECLARE
+	TYPE emp_table IS TABLE OF VARCHAR(50);
+	emp emp_table;
+BEGIN
+	emp := emp_table('Vidhan', 'Vin', 'Reddy');
+
+	-- nested TABLE is unbounded
+	emp.extend;
+	emp(4) := 'Koti';
+
+	emp.extend;
+	emp(5) := 'Manohar';
+
+	-- mutable 
+	emp(2) := 'Ram';
+
+	FOR i IN 1..emp.count() LOOP
+		dbms_output.put_line(emp(i));
+	END LOOP;
+END;
+/
+
+DECLARE
+	TYPE emp_table IS TABLE OF VARCHAR2(30);
+	emp_names emp_table := emp_table();
+	idx NUMBER := 1;
+BEGIN
+	FOR i IN 100..110 LOOP
+		SELECT first_name INTO emp_names(idx)
+		FROM hr.employees
+    	WHERE employee_id = i;
+		
