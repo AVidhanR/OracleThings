9e74f7dda133d449175f5ba7cc0451325bfbe0a2
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2025-02-12 09:34:05 +0530

    Update ref_cursors.sql

--- 07_cursors/ref_cursors.sql
+++ 07_cursors/ref_cursors.sql
@@ -1,6 +1,7 @@
 -- run the below SET command once
 SET serveroutput ON;
 
+-- STRONG CURSOR REF example (RESTRICTIVE CURSOR)
 DECLARE
     -- strong REF CURSOR as it is having a RETURN TYPE
 	TYPE rct_emp IS REF CURSOR return hr.employees%ROWTYPE;
@@ -12,6 +13,38 @@ BEGIN
 		FETCH rc_emp INTO r_emp;
 		EXIT WHEN rc_emp%NOTFOUND;
 		dbms_output.put_line(r_emp.first_name || ' ' || r_emp.last_name);
+	END LOOP;
+	CLOSE rc_emp;
+END;
+/
+
+-- WEAK REF CURSOR example (NON-RESTRICTIVE CURSOR)
+DECLARE
+	-- custome RECORD TYPE
+    TYPE rt_emp IS RECORD (
+    	first_name hr.employees.first_name%TYPE,
+    	last_name hr.employees.last_name%TYPE,
+    	department_name hr.departments.department_name%TYPE
+    );
+    r_emp RT_EMP;
+
+    -- weak REF CURSOR as it is has NO RETURN TYPE
+	TYPE rct_emp IS REF CURSOR;
+	rc_emp RCT_EMP;
+	query VARCHAR2(200);
+BEGIN
+    query := 'SELECT first_name, last_name, department_name
+    FROM hr.employees JOIN hr.departments
+    USING (department_id)';
+
+	-- One can write the above query directly in-place of the 
+	-- query variable
+	OPEN rc_emp FOR query;
+	LOOP
+		FETCH rc_emp INTO r_emp;
+		EXIT WHEN rc_emp%NOTFOUND;
+		dbms_output.put_line(r_emp.first_name || ' ' || r_emp.last_name ||
+            ' under the deapartment ' || UPPER(r_emp.department_name));
 	END LOOP;
 	CLOSE rc_emp;
 END;
