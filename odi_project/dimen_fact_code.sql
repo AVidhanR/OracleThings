b896a9096b4078a634f00e8fc378abe57a9e4bb8
A Vidhan Reddy <itsvidhanreddy@gmail.com>
2025-03-17 20:53:58 +0530

    Create dimen_fact_code.sql

---
+++ odi_project/dimen_fact_code.sql
@@ -0,0 +1,9 @@
+-- run the below commands to create a new schema/user in oracle
+CREATE USER omr IDENTIFIED BY omr;
+GRANT DBA TO omr;
+
+-- connect to the omr schema
+connect omr
+
+-- Dimension tables
+-- 1
