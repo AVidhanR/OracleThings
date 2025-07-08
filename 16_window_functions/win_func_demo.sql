f2a9f81cc88cd1fac9c84f94fe851241b986ee6c
AVidhanR <itsvidhanreddy@gmail.com>
2025-07-08 15:23:35 +0530

    rank, dense_rank and row_numnber demo

--- 16_window_functions/win_func_demo.sql
+++ 16_window_functions/win_func_demo.sql
@@ -16,3 +16,30 @@ SELECT
         ORDER BY e.salary
     ) AS employees_previous_salary
 FROM employees e;
+
+-- get the salary ranking with RANK(), DENSE_RANK() and a small demo of ROW_NUMBER()
+-- ranking is with respect to the each department window
+SELECT
+    e.first_name || ' ' || e.last_name AS full_name,
+    e.salary, (SELECT
+        department_name
+        FROM departments
+        WHERE e.department_id = department_id
+    ) AS department_name,
+    RANK() OVER (
+        PARTITION BY department_id
+        ORDER BY e.salary DESC
+    ) AS salary_ranking,
+    DENSE_RANK() OVER (
+        PARTITION BY department_id
+        ORDER BY e.salary DESC
+    ) AS dense_salary_ranking,
+    ROW_NUMBER() OVER (
+        PARTITION BY department_id
+        ORDER BY e.salary DESC
+    ) AS row_num
+FROM employees e
+WHERE e.salary >= 5000
+ORDER BY row_num, e.salary DESC;
+
+-- great article for understanding window functions: https://medium.com/@hisyam126/oracle-sql-window-functions-0114899572c4
